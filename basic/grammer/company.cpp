1f4df3f8b35492d3e8ed13e8a0dce8200447dfcf
seongpil0948 <seongpil0948@gmail.com>
2021-06-04 15:36:30 +0900

    study CPP

---
+++ basic/grammer/company.cpp
@@ -0,0 +1,20 @@
+#include "companies.h"
+#include <iostream>
+
+int main() {
+    using namespace intellisys;
+    Display();
+    count = 10;
+    printf("\n First Company Name: %s and Count: %d", name, count);
+
+    double a = lhs::display = 350.0;
+    std::cout << "\n Second Company Name:" << lhs::name << "Count:" << a;
+    printf("\n");
+
+    
+
+    
+    
+
+
+};
