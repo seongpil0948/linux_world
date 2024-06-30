2866f1623ba2715f4c75b6d17648f9c58d11c286
최성필 <choeseongpil@choeseongpils-MacBook-Air.local>
2024-06-30 14:09:19 +0900

    feat: centos stream9, ansible

--- local/ubuntu/scripts/build.sh
+++ local/ubuntu/scripts/build.sh
@@ -1,9 +1,16 @@
 docker build . \
+  --file Dockerfile.ubuntu \
   --force-rm \
-  --tag local-ubuntu-image:latest \
+  --tag local-ubuntu-image-1:latest \
   --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
 
 docker build . \
+  --file Dockerfile.ubuntu \
   --force-rm \
   --tag local-ubuntu-image-2:latest \
+  --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
+docker build . \
+  --file Dockerfile.centos \
+  --force-rm \
+  --tag local-centos-image-1:latest \
   --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
